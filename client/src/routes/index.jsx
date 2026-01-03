988961a60564ec600cf79b29578c13ff1431627e
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2026-01-03 11:27:16 +0530

    protect & auth Route client

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,34 +1,61 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
 import Home from "../pages/Home";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 import Dashboard from "../pages/Dashboard";
 import { SendMoney } from "../components/SendMoney";
 
+const ProtectedRoute = () => {
+  const token = localStorage.getItem("token");
 
+  if (!token) {
+    return <Navigate to="signin" replace />;
+  }
+
+  return <Outlet />;
+};
+
+const AuthRoute = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return <Outlet />;
+};
 
 const router = createBrowserRouter([
-    {
-        path : "/",
-        element  : <Home />
+  {
+    path: "/",
+    element: <Home />,
+  },
 
-    },
-    {
-        path : "/signup",
-        element : <SignUp />
-    },
-    {
-        path : "/signin",
-        element : <SignIn />
-    },
-    {
-        path : "/dashboard",
-        element : <Dashboard />
-    },
-    {
-        path : "/send-money",
-        element : <SendMoney />
-    }
-]) 
+  {
+    element: <AuthRoute />,
+    children: [
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+    ],
+  },
+
+  {
+    element: <ProtectedRoute />, 
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/send-money",
+        element: <SendMoney />,
+      },
+    ],
+  },
+]);
 
 export default router;
