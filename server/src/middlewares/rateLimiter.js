16fbda41ceb2d2f7572919dc65ee82492d122d3d
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2026-01-05 13:35:41 +0530

    feat(rate-limit): middleware with express rate limit

---
+++ server/src/middlewares/rateLimiter.js
@@ -0,0 +1,20 @@
+import rateLimit from "express-rate-limit";
+
+export const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15min
+  max: 100,
+  message: {
+    message:
+      "Too many requests from this IP, please try again after 15 minutes",
+  },
+  standardHeaders: true, 
+  legacyHeaders: false, 
+});
+
+export const authLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 5,
+  message: {
+    message: "Too many login attempts. Please try again later.",
+  },
+});
